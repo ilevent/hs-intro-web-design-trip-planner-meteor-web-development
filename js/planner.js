2f871a4f4ed486980ffa8ab641099cc8b6f2b092
ilevent <runwithbooks@gmail.com>
2016-04-02 17:24:54 +0000

    Done.

--- js/planner.js
+++ js/planner.js
@@ -1,1 +1,26 @@
-// Code Your Solution Here
+// name
+// age
+// trip_destination
+// number_of_travelers
+// type_of_trip (relaxing, adventure, etc)
+
+var name = prompt ("What is your name?");
+var age = prompt ("How old are you?");
+var destination = prompt ("Where are you going on this trip?");
+var num_of_travelers = prompt ("How many people are going on this trip (not including you)?");
+var type_of_trip = prompt ("What type of trip is this (ex: business, adventure, relaxing)?");
+
+alert("" + name + " is " + age + " , and going to " + destination + " with " + num_of_travelers + " other people for a(n) " + type_of_trip + " trip. ");
+
+if (type_of_trip == "business"){
+  alert("Business trip tip: ");
+}
+else if (type_of_trip == "adventure"){
+  alert ("b");
+}
+else if (type_of_trip == "relaxing"){
+  alert ("c");
+}
+else {
+  alert ("d");
+}
